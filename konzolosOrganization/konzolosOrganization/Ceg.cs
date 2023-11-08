5e7fc4ea8a140e2ac982ba3756f1037ce386c0de
Zsolesz <72067999+gazdi435@users.noreply.github.com>
2023-11-08 10:31:03 +0100

    Add files via upload

---
+++ konzolosOrganization/konzolosOrganization/Ceg.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konzolosOrganization
+{
+    internal class Ceg
+    {
+        int index;
+        string organizationID;
+        string name;
+        string website;
+        string country;
+        string description;
+        int founded;
+        string industry;
+        int employees;
+
+        public int Index { get => index; set => index = value; }
+        public string OrganizationID { get => organizationID; set => organizationID = value; }
+        public string Name { get => name; set => name = value; }
+        public string Website { get => website; set => website = value; }
+        public string Country { get => country; set => country = value; }
+        public string Description { get => description; set => description = value; }
+        public int Founded { get => founded; set => founded = value; }
+        public string Industry { get => industry; set => industry = value; }
+        public int Employees { get => employees; set => employees = value; }
+
+        public Ceg(int index, string organizationID, string name, string website, string country, string description, int founded, string industry, int employees)
+        {
+            this.index = index;
+            this.organizationID = organizationID;
+            this.name = name;
+            this.website = website;
+            this.country = country;
+            this.description = description;
+            this.founded = founded;
+            this.industry = industry;
+            this.employees = employees;
+        }
+        public Ceg(string sor)
+        {
+            this.index = Convert.ToInt32(sor.Split(";")[0]);
+            this.organizationID = sor.Split(";")[1];
+            this.name = sor.Split(";")[2];
+            this.website = sor.Split(";")[3];
+            this.country = sor.Split(";")[4];
+            this.description = sor.Split(";")[5];
+            this.founded = Convert.ToInt32(sor.Split(";")[6]);
+            this.industry = sor.Split(";")[7];
+            this.employees = Convert.ToInt32(sor.Split(";")[8]);
+        }
+    }
+}
---
+++ konzolosOrganization/konzolosOrganization/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Channels;
+
+namespace konzolosOrganization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ceg> cegek = new List<Ceg>();
+
+            File.ReadAllLines("organizations100.csv").Skip(1).ToList().ForEach(x => cegek.Add(new Ceg(x)));
+
+            //1. feladat
+            Console.Write("1. feladat: ");
+            Console.WriteLine(cegek.Where(x => x.Founded == 2012).Count());
+
+            //2. feladat
+
+            //cegek.Where(x => x.Industry == "Secondary Education").Sum(x => x.Employees) > cegek.Where(x => x.Industry == "Military Industry").Sum(x => x.Employees) ? Console.WriteLine("1") : Console.WriteLine("2");
+            Console.Write("2. feladat: ");
+            if (cegek.Where(x => x.Industry == "Primary / Secondary Education").Sum(x => x.Employees) != cegek.Where(x => x.Industry == "Military Industry").Sum(x => x.Employees))
+            {
+                if (cegek.Where(x => x.Industry == "Primary / Secondary Education").Sum(x => x.Employees) > cegek.Where(x => x.Industry == "Military Industry").Sum(x => x.Employees))
+                {
+                    Console.WriteLine("Primary / Secondary Education-ben többen dolgoznak.");
+                }
+                else
+                {
+                    Console.WriteLine("Military Industry-ban többen dolgoznak.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Egyenlőek!");
+            }
+
+            //3.feladat
+            Console.WriteLine("3. feladat: ");
+            cegek.GroupBy(x =>x.Founded).ToList().OrderBy(x => x.Key).ToList().ForEach(x=> Console.WriteLine($"{x.Key} Évben {x.Count()} cég jött létre."));
+
+            //4. feladat
+            Console.WriteLine("4. feladat: ");
+            cegek.GroupBy(x => x.Country).OrderByDescending(x => x.Count()).ThenBy(x=>x.Key).Take(5).ToList().ForEach(x=>Console.WriteLine($"{x.Key}ba {x.Count()} cég van bejegyezve."));
+
+        }
+    }
+}
